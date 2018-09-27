05537e4e0754b6be2e1e984f1139ffd99c475d0c
danybr0 <dan.covic@outlook.com>
2018-09-27 14:30:58 +0200

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+// basic webserver
+
+// ES2015 syntax
+
+// node_modules
+const http = require('http'); // this module is already included in nodejs
+const fs = require('fs');
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+fs.readFile('index.html', (err, html) => {
+  if(err){
+    throw err;
+  }
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(html);
+    res.end();
+  });
+
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+});
